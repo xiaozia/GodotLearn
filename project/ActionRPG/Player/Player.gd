011318e22a317415646c860d7eba84a85e1ab3a5
xiaozia <814223108@qq.com>
2021-02-04 07:52:31 +0800

    world节点改成ysort

---
+++ project/ActionRPG/Player/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 100
+const FRICTION = 500
+
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body. 
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
+	
