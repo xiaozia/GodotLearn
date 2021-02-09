0f2b91ac50a3dfc1449deda42c00db904212f0c6
xiaozia <814223108@qq.com>
2021-02-10 00:50:22 +0800

    添加了草地

--- project/ActionRPG/Player/Player.gd
+++ project/ActionRPG/Player/Player.gd
@@ -4,6 +4,14 @@ const ACCELERATION = 500
 const MAX_SPEED = 100
 const FRICTION = 500
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 var velocity = Vector2.ZERO
 
 onready var animationPlayer = $AnimationPlayer
@@ -14,7 +22,18 @@ onready var animationState = animationTree.get("parameters/playback")
 func _ready():
 	pass # Replace with function body. 
 
-func _physics_process(delta):
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+			
+		ROLL:
+			pass
+			
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -23,6 +42,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -31,3 +51,13 @@ func _physics_process(delta):
 		
 	velocity = move_and_slide(velocity)
 	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
