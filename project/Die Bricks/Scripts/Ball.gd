597ddc4de2be7aaef0b16a4d2d651dc972051a92
xiaozia <814223108@qq.com>
2021-02-28 16:51:31 +0800

    简单的打砖块代码写完

--- project/Die Bricks/Scripts/Ball.gd
+++ project/Die Bricks/Scripts/Ball.gd
@@ -1,5 +1,8 @@
 extends RigidBody2D
 
+export var speedup = 4
+const MAXSPEED = 400
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,4 +12,16 @@ func _physics_process(delta):
 	
 	for body in bodies:
 		if body.is_in_group("Bricks"):
+			get_parent().score += 5
 			body.queue_free()
+			
+		if body.get_name() == "Paddle":
+			var speed = linear_velocity.length()
+			var direction = position - body.get_node("Ancor").global_position
+			var velocity = direction.normalized()*min(speed+speedup, MAXSPEED)
+			linear_velocity = velocity
+			print(str(speed+speedup))
+			
+	if position.y > get_viewport_rect().end.y:
+		print("Ball died.")
+		queue_free()
--- project/Die Bricks/Scripts/Paddle.gd
+++ project/Die Bricks/Scripts/Paddle.gd
@@ -1,15 +1,17 @@
 extends KinematicBody2D
 
+const ball_scene = preload("res://Mini Scenes/Ball.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _physics_process(delta):
 	var y = position.y
 	var mouse_x = get_viewport().get_mouse_position().x
 	position = Vector2(mouse_x, y)
+	
+func _input(event):
+	if (event is InputEventMouseButton or event is InputEventScreenTouch) and event.pressed:
+		var ball = ball_scene.instance()
+		ball.position = position - Vector2(0, 16)
